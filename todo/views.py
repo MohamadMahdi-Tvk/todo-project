7949d071cbbe3803e7ed356d3dbf8a3145be25b5
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-09-11 13:17:06 +0330

    post api

--- todo/views.py
+++ todo/views.py
@@ -6,8 +6,15 @@ from .models import Todo
 from .serializers import TodoSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def all_todos(request: Request):
-    todos = Todo.objects.order_by('priority').all()
-    todo_serializer = TodoSerializer(todos, many=True)
-    return Response(todo_serializer.data, status.HTTP_200_OK)
+    if request.method == 'GET':
+        todos = Todo.objects.order_by('priority').all()
+        todo_serializer = TodoSerializer(todos, many=True)
+        return Response(todo_serializer.data, status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(None, status.HTTP_400_BAD_REQUEST)
